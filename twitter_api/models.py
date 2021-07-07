1a5eb3d5889d47b868d5999ff46182eccd56ee0e
Nextafari <chinedue16@gmail.com>
2021-07-07 22:30:28 +0100

    Feat: :snake: Used best practice of writing meta class beneath my fields in my models.py

--- twitter_api/models.py
+++ twitter_api/models.py
@@ -3,26 +3,28 @@ from django.db import models
 
 
 class PythonTipSheet(models.Model):
-    class Meta:
-        verbose_name = "Python Tip Sheet"
-        verbose_name_plural = "Python Tips Sheet"
     timestamp = models.CharField(max_length=25, blank=True, null=True)
     python_tip = models.CharField(max_length=140, blank=True, null=True)
     link = models.CharField(max_length=140, blank=True, null=True)
     author = models.CharField(max_length=25, blank=True, null=True)
     published = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name = "Python Tip Sheet"
+        verbose_name_plural = "Python Tips Sheet"
+
     def __str__(self):
         return self.python_tip
 
 
 class PythonTipUserForm(models.Model):
-    class Meta:
-        verbose_name = "Python Tip User Form"
-        verbose_name_plural = "Python Tips User Forms"
     python_tip = models.CharField(max_length=140, blank=True, null=True)
     twitter_handle = models.CharField(max_length=20, blank=True, null=True)
     email = models.CharField(max_length=40, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Python Tip User Form"
+        verbose_name_plural = "Python Tips User Forms"
 
     def __str___(self):
         return self.email
